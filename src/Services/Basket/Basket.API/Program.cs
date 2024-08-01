001d78129634fbc9fdd887529b67521272a37758
vijaychovatiya <vijaychovatiya@hotmail.com>
2024-08-01 20:19:12 +0530

    microservies

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using HealthChecks.UI.Client;
 using BuildingBlocks.Messaging.MassTransit;
+using Discount.Grpc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -31,7 +32,23 @@ builder.Services.AddStackExchangeRedisCache(options =>
     options.Configuration = builder.Configuration.GetConnectionString("Redis");
     //options.InstanceName = "Basket";
 });
+//Grpc Services
+builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(options =>
+{
+    options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]!);
+})
+.ConfigurePrimaryHttpMessageHandler(() =>
+{
+    var handler = new HttpClientHandler
+    {
+        ServerCertificateCustomValidationCallback =
+        HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+    };
 
+    return handler;
+});
+
+//Async Communication Services
 builder.Services.AddMessageBroker(builder.Configuration);
 //Cross-Cutting Services
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
